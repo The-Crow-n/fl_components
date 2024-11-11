a59b3577b78e3ea65ad14699415de47de05f4b9c
Alberto Canalis Saez <albertocanalissaez@gmail.com>
2024-11-11 10:52:39 +0100

    fourteenth commit

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   // Color primario para la aplicación
-  static const Color primary = Colors.deepOrange;
+  static const Color primary = Colors.green;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       // Color primario para la aplicación
       //primaryColor: Colors.deepOrange,
       // AppBar Theme
-      appBarTheme: const AppBarTheme(color: primary),
+      appBarTheme: const AppBarTheme(
+          color: primary,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 24)),
       listTileTheme: const ListTileThemeData(iconColor: primary));
 }
