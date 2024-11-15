1ed01f5ee4f2d71bb48eb17096c5539c5b0fddbd
Alberto Canalis Saez <albertocanalissaez@gmail.com>
2024-11-15 09:15:15 +0100

    twenty-second commit

--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -9,13 +9,14 @@ class CustomCardTipo2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Column(
         children: <Widget>[
           const FadeInImage(
             image: NetworkImage(
                 'https://cdn.muni.cz/media/rss/29516-2024_7_11__Phd_Awards_titulka.jpg?mode=crop&heightratio=0.5'),
             placeholder: AssetImage('assets/jar-loading.gif'),
-            //width: double.infinity,
+            width: double.infinity,
             height: 250,
             fit: BoxFit.cover,
             fadeInDuration: Duration(milliseconds: 1500),
