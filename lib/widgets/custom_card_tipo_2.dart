90da0718b69c421ba49b3c76c29fc9e7c3438725
Alberto Canalis Saez <albertocanalissaez@gmail.com>
2024-11-15 09:12:42 +0100

    twenty-first commit

--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -1,4 +1,3 @@
-import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardTipo2 extends StatelessWidget {
@@ -8,12 +7,25 @@ class CustomCardTipo2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
+      clipBehavior: Clip.antiAlias,
       child: Column(
-        children: [
-          Image(
-              image: NetworkImage(
-                  'https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?cs=srgb&dl=pexels-eberhardgross-443446.jpg&fm=jpg'))
+        children: <Widget>[
+          const FadeInImage(
+            image: NetworkImage(
+                'https://cdn.muni.cz/media/rss/29516-2024_7_11__Phd_Awards_titulka.jpg?mode=crop&heightratio=0.5'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            //width: double.infinity,
+            height: 250,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 1500),
+            fadeOutDuration: Duration(milliseconds: 800),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(top: 15, bottom: 15, right: 8),
+            child: const Text('Albaricoque'),
+          )
         ],
       ),
     );
