0cc43a354b036540949a5f15cd0b817393fdc38f
Alberto Canalis Saez <albertocanalissaez@gmail.com>
2024-11-25 10:50:32 +0100

    displayDialog + showDialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,17 +3,29 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+              title: Text('Alerta'),
+              content: Text('Este es el contenido del mensaje de alerta'));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
           child: ElevatedButton(
-            onPressed: () {},
-            child: const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text("Mostrar alerta", style: TextStyle(fontSize: 20),),
-            )
-            ),
+              onPressed: () => displayDialog(context),
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  "Mostrar alerta",
+                  style: TextStyle(fontSize: 20),
+                ),
+              )),
         ),
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.close),
