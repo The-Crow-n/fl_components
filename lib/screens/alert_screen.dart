b8d48a43c0e41e651710fcc91cfeb7a3f413865a
Alberto Canalis Saez <albertocanalissaez@gmail.com>
2024-11-25 11:04:22 +0100

    AlertDialog ---> shape

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -8,9 +8,10 @@ class AlertScreen extends StatelessWidget {
         barrierDismissible: true, // Cuando está en false, no deja salir del diálogo clicando en el sombreado.
         context: context,
         builder: (context) {
-          return const AlertDialog(
-              title: Text('Alerta'),
-              content: Column(
+          return AlertDialog(
+              title: const Text('Alerta'),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(0)),
+              content: const Column(
                 mainAxisSize: MainAxisSize.min, // Si no se especifica, por defecto viene al máximo.
                 children: [
                   Text('Este es el contenido del mensaje de alerta'),
@@ -18,7 +19,13 @@ class AlertScreen extends StatelessWidget {
                   FlutterLogo(size: 100,)
                 ],
               ),
-              );
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cerrar')
+                  ),
+              ],
+            );
         });
   }
 
